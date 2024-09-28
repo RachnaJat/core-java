3ff18c35b4cdf4cf23193ed4df438d1d52259734
rachna jat <jatrachna941@gmail.com>
2024-09-27 22:38:29 -0700

    commit msg changed output msg

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("SIMPLE JAVA PROGRAM");
+        System.out.println("WELCOME TO MY FIRST JAVA PROGRAM ON GITHUB");
     }
     
 }
