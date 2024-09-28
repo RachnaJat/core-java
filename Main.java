f12b4ac3dce522cfbbeb69870abdb612ec00ff80
rachna jat <jatrachna941@gmail.com>
2024-09-28 02:18:35 -0700

    3 different print methods in java

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        /*In JAVA , WE CAN SIMPLY USE 3 DIFFERENT PRINT METHODS TO SEND OUTPUT TO STANDARD OUTPUT (SCREEN)
+                   OR In Simple Words We Can Say Java Provides Us 3 Methods To Print Output
+          System.out.println();  or
+          System.out.print();    or
+          System.out.printf();
+         */
         System.out.println("WELCOME TO MY FIRST JAVA PROGRAM ON GITHUB");
     }
     
