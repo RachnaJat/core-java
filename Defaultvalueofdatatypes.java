a41ecc857347f85ea153b3cd8064b99fd1659b05
rachna jat <jatrachna941@gmail.com>
2024-09-28 19:53:50 -0700

    Default value of all primitive data types

---
+++ Defaultvalueofdatatypes.java
@@ -0,0 +1,27 @@
+public class Defaultvalueofdatatypes {
+
+    static   byte a;
+    static  short b;
+    static  int c;
+    static  long d;
+    static  float e;
+   static  double f;
+   static   char g;
+   static   boolean h;
+    public static void main(String[] args)
+     {
+        
+            //DEFAULT VALUE OF ALL PRIMITIVE DATA TYPES :-
+
+        System.out.println("default value of byte= "+a);         //0
+        System.out.println("default value of short= "+b);        //0
+        System.out.println("default value of int= "+c);          //0
+        System.out.println("default value of long= "+d);         //0
+        System.out.println("default value of float= "+e);        //0.0
+        System.out.println("default value of double= "+f);       //0.0
+        System.out.println("default value of char= "+g);         // 
+        System.out.println("default value of boolean= "+h);      // false
+   
+    }
+    
+}
