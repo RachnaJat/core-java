f533949aa4b89a3b4cd13d30faad0ec5a4e2343e
Rachna jat <jatrachna941@gmail.com>
2024-10-01 07:51:28 -0700

    Table of 2 using for loop

---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main(String[] args) {
+        System.out.println("Table of 2 using for loop");
+        for(int i =1; i<=10; i++)
+        {
+            System.out.println("2 * "+i+ " = "+(i*2));
+
+        }
+    }
+    
+}
